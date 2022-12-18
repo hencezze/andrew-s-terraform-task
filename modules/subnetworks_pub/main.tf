f6484f4247e9d88b10a01d38e8c6c72c89dc027e
hencezze <104189173+hencezze@users.noreply.github.com>
2022-12-18 18:59:24 +0300

    Update main.tf

--- modules/subnetworks_pub/main.tf
+++ modules/subnetworks_pub/main.tf
@@ -13,7 +13,7 @@ resource "google_compute_subnetwork" "subnetworks_public" {
   name          = each.key
   ip_cidr_range = each.value
   region        = var.region
-  network       = var.network1
+  network       = var.network
 
 }
 
